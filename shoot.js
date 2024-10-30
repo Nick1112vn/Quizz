e69e2776550a132b598c1a8f6336c4af6fec9ddc
Nick1112vn <149287967+Nick1112vn@users.noreply.github.com>
2024-10-30 22:53:08 +0700

    a

--- shoot.js
+++ shoot.js
@@ -1,5 +1,5 @@
 let lastX, lastY, lastZ;
-let shakeThreshold = 15; // Ngưỡng để xác định rung mạnh hay yếu
+let shakeThreshold = 2; // Ngưỡng để xác định rung mạnh hay yếu
 let shakeTimeout;
 alert("shot")
 // Hàm xử lý khi lắc điện thoại
